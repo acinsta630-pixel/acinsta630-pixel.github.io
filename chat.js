9ab524d38cf9e3fb36b61e8ca396293943b17fc7
acinsta630-pixel <acinsta630@gmail.com>
2026-07-22 20:09:06 -0400

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  try {
+    const response = await fetch("https://acinsta630-pixel-github-io.vercel.app/", {
+      method: "POST",
+      headers: {
+        "Authorization": req.headers.authorization || "",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
